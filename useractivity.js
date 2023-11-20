4cbd8fe3934d39f0ced4719418658b29cf04923f
Abdulajishs <abdulajeeshsb@gmail.com>
2023-11-20 18:22:42 +0530

    update time

---
+++ useractivity.js
@@ -0,0 +1,56 @@
+const posts = [{title: 'POST1'},{title: 'POST2'}];
+function printPost() {
+    posts.forEach((post) => {
+        console.log(post.title)
+    })
+}
+
+function create3rdPost() {
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            posts.push({title: 'POST3'});
+            resolve()
+        }, 2000)
+    }) 
+}
+function create4thPost() {
+    console.log(`Before creating post4, user lasttimeactivity = ${new Date()}`);
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            posts.push({title: 'POST4'});
+            resolve();
+        }, 4000)
+    }) 
+}
+
+function updateLastUserActivityTime (){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            let lastactivitytime = new Date()
+            resolve(lastactivitytime )
+        },1000)
+    })
+}
+
+function deletePost() {
+    return new Promise((resolve, reject) => {   
+        setTimeout(()=>{
+            posts.pop()
+            resolve()
+        },1000)
+    })
+}
+
+Promise.all([create3rdPost() ,create4thPost(),updateLastUserActivityTime()]).then((value)=>{
+    console.log("After creating post 4 >>>>>>>");
+    console.log(posts);
+    console.log(`User last time activity ${value}`);
+    
+})
+.then(()=>{
+    deletePost();
+    console.log(posts);
+})
+
+
+
